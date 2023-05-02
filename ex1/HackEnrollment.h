758ccd3218548f3525e74dfa11f0455b52adf6a4
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-05-02 16:59:11 +0300

    completed the header file for HackEnrollment

--- ex1/HackEnrollment.h
+++ ex1/HackEnrollment.h
@@ -4,11 +4,33 @@
 
 #include <stdio.h>
 
-typedef struct EnrollmentSystem {
+typedef struct Student_t {
+	char* name;
+	int id;
+} *Student;
 
-};
+typedef struct Course_t {
+	int courseNumber;
+	int size;
+} *Course;
+
+typedef struct Hacker_t {
+	int id;
+	int* courseNumbers;
+	int* friendsIds;
+	int* rivalsIds;
+} *Hacker;
+
+typedef struct EnrollmentSystem_t {
+	Student* studentsArray;
+	Course* coursesArray;
+	Hacker* hackersArray;
+}EnrollmentSystem;
 
 EnrollmentSystem createEnrollment(FILE* students, FILE* courses, FILE* hackers);
+EnrollmentSystem readEnrollment(EnrollmentSystem sys, FILE* queues);
+void hackEnrollment(EnrollmentSystem sys, FILE* out);
+
 
 #endif /*HACK_ENROLLMENT_H*/
 
