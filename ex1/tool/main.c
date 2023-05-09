5939f9cba738f794bb7d4bc771b8150645484ebf
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-05-09 22:12:24 +0300

    ecrit le main comme ruben

--- ex1/tool/main.c
+++ ex1/tool/main.c
@@ -3,21 +3,46 @@
 #include "HackEnrollment.h"
 #include <stdlib.h>	
 
+void hackEnrollmentProgram(char* students, char* courses, char* hackers, char* queues, char* out)
+{
+	FILE* studentsFile = fopen(students, "r");
+	FILE* coursesFile = fopen(courses, "r");
+	FILE* hackersFile = fopen(hackers, "r");
+	FILE* queuesFile = fopen(queues, "r");
+	FILE* outFile = fopen(out, "w");
+	EnrollmentSystem sys = createEnrollment(studentsFile, coursesFile, hackersFile);
+	sys = readEnrollment(sys, queuesFile);
+	hackEnrollment(sys, outFile);
+	fclose(studentsFile);
+	fclose(coursesFile);
+	fclose(hackersFile);
+	fclose(queuesFile);
+	fclose(outFile);
+}	
+
 int main(int argc, char** argv)
 {
-	printf("sdd");
-	FILE* students = fopen(argv[2], "r");
-	FILE* courses = fopen(argv[3], "r");
-	FILE* hackers = fopen(argv[4], "r");
-	FILE* queues = fopen(argv[5], "r");
-	FILE* out = fopen(argv[6], "w");
-	EnrollmentSystem sys = createEnrollment(students, courses, hackers);
-	sys = readEnrollment(sys, queues);
-	hackEnrollment(sys, out);
-	fclose(students);
-	fclose(courses);
-	fclose(hackers);
-	fclose(queues);
-	fclose(out);
+	char* students = NULL;
+	char* courses = NULL;
+	char* hackers = NULL;
+	char* queues = NULL;
+	char* out = NULL;
+	if (argc == 7)
+	{
+		students = argv[2];
+		courses = argv[3];
+		hackers = argv[4];
+		queues = argv[5];
+		out = argv[6];
+	}
+	else 
+	{
+		students = argv[1];
+		courses = argv[2];
+		hackers = argv[3];
+		queues = argv[4];
+		out = argv[5];
+	}
+	hackEnrollmentProgram(students, courses, hackers, queues, out);
 	return 0;
 }
